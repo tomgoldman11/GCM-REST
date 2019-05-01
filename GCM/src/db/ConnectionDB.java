5222ee1c56452aa8cb1722bc6512f8eb54a92ea8
Meshi <mesh2470@gmail.com>
2019-05-01 14:23:54 +0300

    updateserver

--- GCM/src/db/ConnectionDB.java
+++ GCM/src/db/ConnectionDB.java
@@ -18,118 +18,85 @@ public class ConnectionDB {
     static private final String DB_URL = "jdbc:mysql://remotemysql.com/" + DB + "?useSSL=false";
     static private final String USER = "c4EgFjQoFL";
     static private final String PASS = "lcOQ5ludsS";
-    static public String ansUser;
-    public String Connect2db(Object msg) throws SSLException {
-        Connection conn = null;
-        Statement stmt = null;
-        try {
-            Class.forName(JDBC_DRIVER);
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            stmt = conn.createStatement();
+    Connection conn = null;
+    Statement stmt = null;
+    public String ansUser;
+    public void init() {
+    	
+    	
+         try {
+             Class.forName(JDBC_DRIVER);
+             conn = DriverManager.getConnection(DB_URL, USER, PASS);
+             stmt = conn.createStatement();
+         } catch (SQLException se) {
+             se.printStackTrace();
+             System.out.println("SQLException: " + se.getMessage());
+             System.out.println("SQLState: " + se.getSQLState());
+             System.out.println("VendorError: " + se.getErrorCode());
+         } catch (Exception e) {
+             e.printStackTrace();
+         }
+    }
+    public void close() {
+    	
+    	try {
+            if (stmt != null)
+                stmt.close();
+            if (conn != null)
+                conn.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        }
+    }
+
+    
+    public String getUserDetails(Object msg) throws SSLException, SQLException {
+       
 
             System.out.println("\t============");
-            
-            
-
-            /* exercize a */
-            
             System.out.println("----------------------");
             
-            ResultSet ansus = stmt.executeQuery(msg.toString());
+            ResultSet ansus = stmt.executeQuery(msg.toString().substring(1));
             while (ansus.next()) {
             	String userID = ansus.getString("userID");
             	String password = ansus.getString("password");
             	String registerDate = ansus.getString("registerDate");
-            	ansUser = userID + " " + password;
+            	ansUser ="@ "+ userID + " " + password ;
             }
-            
 
-//            /* exercize b */
-//            
-//            sql = "SELECT price " + "FROM flights " + "WHERE num=387";
-//            ResultSet ans = stmt.executeQuery(sql);
-//            ans.next();
-//            System.out.println("new price is: " + ans.getInt("price"));
-//
-//
-//            /* exercize c */
-//            
-//            Statement stmt2 = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
-//            sql = "SELECT * " + "FROM flights " + "WHERE distance>1000";
-//            ResultSet ans2 = stmt2.executeQuery(sql);
-//            while (ans2.next()){
-//                ans2.updateFloat("price", ans2.getFloat("price")+50);
-//                ans2.updateRow(); // update the change in the database.
-//            }
-//            ans2.close();
-//            stmt2.close();
-//
-//            /* exercize d */
-//            
-//            PreparedStatement updateSales = conn.prepareStatement("UPDATE flights "+ " SET price = price + 50 WHERE distance > ? ");
-//                updateSales.setInt(1, 1000); 
-//                updateSales.executeUpdate(); // run the query only on flights which their distance is bigger than 1000.
-//
-//
-//            sql = "SELECT * FROM flights";
-//            ResultSet rs = stmt.executeQuery(sql);
-//            while (rs.next()) { // printing row by row of the database.
-//                int num = rs.getInt("num");
-//                String origin = rs.getString("origin");
-//                String destination = rs.getString("destination");
-//                int distance = rs.getInt("distance");
-//                int price = rs.getInt("price");
-//
-//                System.out.format("Number %5s Origin %15s destinations %18s Distance %5d Price %5d\n", num, origin, destination, distance, price);
-//            }
-//
-//            System.out.println("\t============");
-//
-//            sql = "SELECT origin, destination, distance, num FROM flights";
-//            rs = stmt.executeQuery(sql);
-//            while (rs.next()) {
-//                String origin = rs.getString("origin");
-//                String destination = rs.getString("destination");
-//                int distance = rs.getInt("distance");
-//
-//                System.out.print("From: " + origin);
-//                System.out.print(",\tTo: " + destination);
-//                System.out.println(",\t\tDistance: " + distance);
-//            }
-//
-//            System.out.println("\t============");
-//
-//            sql = "SELECT origin, destination FROM flights WHERE distance > ?";
-//            PreparedStatement prep_stmt = conn.prepareStatement(sql);
-//            prep_stmt.setInt(1, 200);
-//            rs = prep_stmt.executeQuery();
-//            while (rs.next()) {
-//                String origin = rs.getString("origin");
-//                System.out.println("From: " + origin);
-//            }
-
-//            rs.close();
-            stmt.close();
-            conn.close();
-            
-
-        } catch (SQLException se) {
-            se.printStackTrace();
-            System.out.println("SQLException: " + se.getMessage());
-            System.out.println("SQLState: " + se.getSQLState());
-            System.out.println("VendorError: " + se.getErrorCode());
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (stmt != null)
-                    stmt.close();
-                if (conn != null)
-                    conn.close();
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }
-        }
+            close();
+   
         return ansUser;
     }
+    public String getCustomerDetails(Object msg) throws SSLException, SQLException {
+        
+
+        System.out.println("\t============");
+        System.out.println("customerDB");
+        
+        ResultSet ansus = stmt.executeQuery(msg.toString().substring(1));
+        while (ansus.next()) {
+        	
+        	String userID = ansus.getString("userID");
+        	String cusID = ansus.getString("cusID");
+	      	String purchases = ansus.getString("purchases");
+	      	ansUser = "% " + userID + " " + cusID + " " + purchases;
+        }
+
+        close();
+
+
+    return ansUser;
+    }
+    
+    public void setCustoerDetail(Object msg)throws SSLException, SQLException {
+    	
+    		System.out.println(msg.toString().substring(1));
+    		System.out.println("customerUpdate");
+    		PreparedStatement update = conn.prepareStatement(msg.toString().substring(1));
+    		update.executeUpdate();
+    		update.close();
+    		return;
+    }
+    	
 }
